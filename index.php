e74d18b7a5adc4834ae5b2b5f6d72db19f3a900b
Sankalp <sankalp@sportskeeda.com>
2015-12-21 10:32:31 +0530

    Little Modularity

--- index.php
+++ index.php
@@ -10,13 +10,11 @@ if ($handle) {
     while (($line = fgets($handle)) !== false) {
     	$line = explode(" ", $line);
         $newer_second = date("h:i:s", strtotime($line[0]));
-        echo "Compare $newer_second with $older_second";
+        //echo "Compare $newer_second with $older_second";
         if($newer_second != $older_second) {
-         echo '<span class = "class'. ($older_class ++) % 2 .'">';
+	 $older_class  = ($older_class + 1) % 2;
         }   
-        else {
-         echo '<span class = "class'. $older_class .'">';
-        }
+        echo '<span class = "class'. $older_class .'">';
         $older_second = $newer_second;
 	echo get_line_from_raw($line).'</span><br /><br />';
     }
@@ -27,11 +25,14 @@ if ($handle) {
 } 
 
 function get_line_from_raw($line) {
- return date("D d M Y h:i:s", strtotime($line[0])).' | '.$line[3].' | '.(floatval($line[4]) * 1000).' + '.(floatval($line[5]) * 1000).' + '.(floatval($line[6]) * 1000).
-        ' | '.
-        $line[7].' '.$line[8].' | '.$line[9].' &darr; | '.$line[10].' &uarr; '.
+ $status_code_to_color_class = array('200' => 'good', '301' => 'warn', '302' => 'warn', '404' => 'err', '500' => 'err');
+ $return_string = date("D d M Y h:i:s", strtotime($line[0])).' | '.$line[3].' | '.(floatval($line[4]) * 1000).' + '.(floatval($line[5]) * 1000).' + '.(floatval($line[6]) * 1000).' | ';
+ if(isset($status_code_to_color_class[$line[7]])) {
+ }
+        $return_string .= $line[7].' '.$line[8].' | '.$line[9].' &darr; | '.$line[10].' &uarr; '.
         ' | <b>'.$line[11].
         '</b> '.$line[12].' | ';
+ return $return_string;
 }
 ?>
 </pre>
