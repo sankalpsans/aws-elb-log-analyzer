40bf3eb79d939903c9b8f680bab05083ff3d8849
Sankalp <sankalp@sportskeeda.com>
2015-12-23 20:27:20 +0530

    Added dependency for AWS SDK, so that log files can be read directly without having to download them manually every time

--- config.php
+++ config.php
@@ -1,3 +1,4 @@
 <?php
- $filename = './246435084856_elasticloadbalancing_ap-southeast-1_sportskeeda-new_20151218T1040Z_54.169.126.87_40mdv2b8.log';
+// $filename = './246435084856_elasticloadbalancing_ap-southeast-1_sportskeeda-new_20151218T1040Z_54.169.126.87_40mdv2b8.log';
+$filename='./work/246435084856_elasticloadbalancing_ap-southeast-1_sportskeeda-new_20151223T1315Z_54.255.208.168_4mmmykgo.log';
  $status_code_to_color_class = array('200' => 'good', '301' => 'warn', '302' => 'warn', '304' => 'warn', '404' => 'err', '500' => 'err');
